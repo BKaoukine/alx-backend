d65d81b1341607515eeb9da455fa55671a1d61b6
bkaoukine <b.koukin@gmail.com>
2024-06-26 17:58:02 +0100

    0. Basic dictionary

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-""" task 0 in cache module """
-BaseCaching = __import__("base_caching").BaseCaching
+"""
+BasicCache module.
+
+This module provides a basic caching system implementation.
+"""
+
+BaseCaching = __import__('BaseCaching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """basic cache class"""
+    """BasicCache defines a simple caching system."""
 
     def put(self, key, item):
-        """put the cache in pairs"""
+        """
+        Add an item in the cache.
+
+        Args:
+            key: The key under which the item will be stored.
+            item: The item to be stored in the cache.
+        """
         if key and item:
             self.cache_data[key] = item
 
     def get(self, key):
-        """get the cached values"""
+        """
+        Get an item from the cache by key.
+
+        Args:
+            key: The key of the item to be retrieved.
+
+        Returns:
+            The cached item, or None if the key is not in the cache.
+        """
         return self.cache_data.get(key)
